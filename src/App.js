e215892aed2b007a71a82f85ee5d6cf4e7af684b
Roman Yarovoy <roman@booma.co.il>
2020-08-23 12:56:34 +0300

    add_redp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
+import { Cards } from './components/Cards'
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
-function App() {
+
+const App = () => {
+  const datetime = new Date()
+  const [recovered, handlerecovered] = useState('')
+  const [infected, handleinfected] = useState('')
+  const [depths, handledepths] = useState('')
+  const [countries, handlecountries] = useState('')
+
+
+  useEffect(() => {
+    axios.get(`https://covid19.mathdro.id/api`)
+      .then(res => {
+        const recoveredpeople = res.data.recovered.value;
+        const infectedpeople = res.data.confirmed.value;
+        const depthspeople = res.data.deaths.value;
+        handlerecovered(recoveredpeople)
+        handleinfected(infectedpeople)
+        handledepths(depthspeople)
+      })
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {console.log(countries)}
+      <Title>Covid Tracker</Title>
+      <Cards date={datetime} recovered={recovered} infected={infected} depths={depths} />
+      <div className="container-label">
+        <LabelinfoText>Infected</LabelinfoText>
+        <LabelinfoText>deaths</LabelinfoText>
+      </div>
+      <select>
+        <option>Global</option>
+      </select>
     </div>
   );
 }
 
 export default App;
+const Title = styled.h2`
+padding-top:7rem;
+font-size:3rem;
+text-align:center;
+`
+const Select = styled.select`
+text-align:center;
+`;
+const LabelinfoText = styled.span`
+display:block;
+font-size:2rem;
+&::after{
+content:'';
+display: block;
+width:10rem;
+height:3vh;
+background:red;
+text-align:center;
+}
+`
+
---
+++ src/components/Cards.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components'
+
+
+
+export const Cards = (props) => {
+  return (
+    <Wrapper>
+      <Card>
+        <TitleCard>Infected</TitleCard>
+        <Number>{props.infected}</Number>
+        <Date>date {props.date.toString()}</Date>
+        <CardText>number of active Cases of COVID-19</CardText>
+      </Card>
+      <Card>
+        <TitleCard>Recovered</TitleCard>
+        <Number>{props.recovered}</Number>
+        <Date>date {props.date.toString()}</Date>
+        <CardText>number of recovered Cases of COVID-19</CardText>
+      </Card>
+      <Card>
+        <TitleCard>Deaths</TitleCard>
+        <Number>{props.depths}</Number>
+        <Date>date {props.date.toString()}</Date>
+        <CardText>number of deaths Cases of COVID-19</CardText>
+      </Card>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+padding-top:1rem;
+display:flex;
+justify-content:center;
+`;
+const Card = styled.div`
+padding-bottom:2rem;
+padding-left:2rem;
+padding-top:2rem;
+padding-right:2rem;
+border-radius:1rem;
+margin:2rem;
+border:1px solid #ebebeb;
+
+`;
+const TitleCard = styled.h3`
+padding-top:1rem;
+font-size:2rem;
+`;
+const Date = styled.span`
+max-width:300px;
+padding-top:1rem;
+display:block;
+font-size:2rem;
+`;
+const CardText = styled.p`
+padding-top:1rem;
+font-size:2rem;
+`;
+const Number = styled.label`
+display:block;
+padding-top:2rem;
+font-weight:bold;
+font-size:2.5rem;
+`;
